a8dd6d58b7f8ea389e0fec3867ffd529a2fa09f3
LingLambda <abc1514671906@163.com>
2026-08-14 18:24:05 +0800

    发布 RelifeLuna 0.0.2

--- client/relife-icon.vue
+++ client/relife-icon.vue
@@ -1,6 +1,8 @@
 <template>
-  <svg viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-    <path d="M12 12a4 4 0 1 0 0-8 4 4 0 0 0 0 8Zm0 2c-3.33 0-10 1.67-10 5v3h20v-3c0-3.33-6.67-5-10-5Z" />
-    <path d="m17.5 1.5 1.1 2.4 2.4 1.1-2.4 1.1-1.1 2.4-1.1-2.4-2.4-1.1 2.4-1.1 1.1-2.4Z" />
+  <svg class="k-icon" xmlns="http://www.w3.org/2000/svg" viewBox="1.1 1.1 21.8 21.8" aria-hidden="true">
+    <g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.6">
+      <path d="M4 21v-1c0-3.31 2.69-6 6-6h4c3.31 0 6 2.69 6 6v1" />
+      <path d="M12 11c-2.21 0-4-1.79-4-4c0-2.21 1.79-4 4-4c2.21 0 4 1.79 4 4c0 2.21-1.79 4-4 4Z" />
+    </g>
   </svg>
 </template>
